358e03cd84b26c1dc74abdafa677baf564854ad5
Peter Jang <peter.x.jang@gmail.com>
2018-08-05 09:50:00 -0500

    Use v-model and v-on:click to save new recipe to backend

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="home">
+    <h1>New Recipe</h1>
+    <div>
+      title: <input type="text" v-model="title">
+      chef: <input type="text" v-model="chef">
+      ingredients: <input type="text" v-model="ingredients">
+      directions: <input type="text" v-model="directions">
+      prep_time: <input type="text" v-model="prep_time">
+      image_url: <input type="text" v-model="image_url">
+      <button v-on:click="createRecipe()">Create recipe</button>
+    </div>
     <h1>Recipes</h1>
     <div v-for="recipe in recipes">
       <h2>{{ recipe.title }}</h2>
@@ -19,7 +29,13 @@ var axios = require("axios");
 export default {
   data: function() {
     return {
-      recipes: []
+      recipes: [],
+      title: "",
+      chef: "",
+      ingredients: "",
+      directions: "",
+      prep_time: "",
+      image_url: ""
     };
   },
   created: function() {
@@ -30,7 +46,24 @@ export default {
       }.bind(this)
     );
   },
-  methods: {},
+  methods: {
+    createRecipe: function() {
+      var params = {
+        input_title: this.title,
+        input_chef: this.chef,
+        input_ingredients: this.ingredients,
+        input_directions: this.directions,
+        prep_time: this.prep_time,
+        input_image_url: this.image_url
+      };
+      axios.post("http://localhost:3000/api/v1/recipes", params).then(
+        function(response) {
+          console.log("the response is", response);
+          this.recipes.push(response.data);
+        }.bind(this)
+      );
+    }
+  },
   computed: {}
 };
 </script>
